aeb5d2676e5dcde208942e1d637423108e4d2130
thien <dvkndn@gmail.com>
2016-06-17 23:13:59 +0700

    align withdrawal

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
   var button = $('#login');
-  var quoineHost = "https://sandbox.quoine.com",
-      jbitsHost = "https://jbit.quoine.com";
-  var userId = "7605",
-      secret = "/1HwnL1u/DzodLIXmDXYvBtmB7inr/xqKg+F8J8QKVQR7J9pVOg0boH9Lm9eFHyy9h8rfQI7h6W3vdOcL7TWrQ==";
+  var quoineHost = "https://staging1.quoine.com",
+      jbitsHost = "http://localhost:3000";
+
+  // ASANO
+  // var userId = "8721",
+  //     secret = "A367oAQNEH0BhA3YCjmO1y4hDCpwc3Nt0WdaPY2t1gD+tOTB2WOK4h400TCs+NbBigMsd8LZNnvP0uTzDF0qtA==";
+
+  // ASANO 2
+  // var userId = "8542",
+  //     secret = "eMNxmErhooEYjCVTfU5HX4EbNBZESprP0kdaeyttFW4+JLT03j5ZPB46F79O2X2xqeTDz7vEIlKiZDRZtqd0jw==";
+
+  // MARIO
+  // var userId = "5264",
+  //     secret = "aaqRcNdJox8Js4mWYI2KO8MAhu/rDJ3DrP0oe5yljyd0qAzYAKLhzy6FdIuFgANFVhBT6h5muZ+BCqzqtPBz2Q==";
+
+  // ME
+  var userId = "5521",
+      secret = "FRQtr1NvxfI3MVtitozN3RgCfFwZC8PQfJNnHbH672rc7+NpmHQGYc94b+nhK0KMbWLuU7Ps+ccs7msOwlgBSA==";
+
+  // CHINH
+  // var userId = "5712",
+  //     secret = "wmCIAW/DSwaLTB4f8L023vkRnpF5CwIiHYL0U+1KCJG9LedS/q8j42ohvo80hbcIOofxKXpIHsruCKYNsE5qfA==";
+
+  // CHINH 2
+  // var userId = "5607",
+  //     secret = "iP9TgR+0S1L3CG4NahlgMENu1lZK+9YQqlzYNuyDAtPowqGxGhqQl46RBE6nPsPDaGJbwfLbuJ960jhL96krTw==";
+
+  // JBIT
+  // var userId = "16892",
+  //     secret = "/pY6BZz0D5RnrajQmvq8X+tUYp/LegPOIxZQa381s58SQHxLfcm/s8fHAPHz2eu30gKzJJTuYXSWMnRRNCa8mA==";
 
   //Sign the request for Authentication
   $.ajaxSetup({
@@ -19,7 +45,8 @@ $(document).ready(function() {
     //Request login token
     $.getJSON(quoineHost + "/cors_auth", function(response) {
       //Use token to login to trading dashboard
-      window.location = jbitsHost + "/app/#/app/direct-auth?token="+response.encrypted
+      window.open(jbitsHost + "/#/auth?token=" + response.encrypted, '_blank', 'width=1280,height=800,toolbar=0,location=0,menubar=0');
+      // window.location = jbitsHost + "/#/auth?token="+response.encrypted
     });
   });
 });
