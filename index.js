28372455c25df20dd0e2efa315c3d05a8e5d989e
thien <dvkndn@gmail.com>
2016-07-15 15:10:58 +0700

    add prompt if neccessary

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
 $(document).ready(function() {
   var button = $('#login');
   var quoineHost = "https://staging1.quoine.com",
+  // var quoineHost = "https://jbits-api.quoine.com",
       jbitsHost = "http://localhost:3000";
 
-  // ASANO
-  // var userId = "8721",
-  //     secret = "A367oAQNEH0BhA3YCjmO1y4hDCpwc3Nt0WdaPY2t1gD+tOTB2WOK4h400TCs+NbBigMsd8LZNnvP0uTzDF0qtA==";
+  // staging
+  var userId = "9143",
+      secret = "OkZax807AbGraK8IM8Zii1eznQ8rgQW7QPPGvx1Iejo6BQYbFPmVBtjEZnjDTcX49PGBuck/Yr9NnWF59qoi+g==";
 
-  // ASANO 2
-  // var userId = "8542",
-  //     secret = "eMNxmErhooEYjCVTfU5HX4EbNBZESprP0kdaeyttFW4+JLT03j5ZPB46F79O2X2xqeTDz7vEIlKiZDRZtqd0jw==";
+  // quoineHost = prompt("Please enter quoine host", quoineHost);
+  // jbitsHost = prompt("Please enter url", jbitsHost);
+  // tokenId = prompt("Please enter user id", tokenId);
+  // secret = prompt("Please enter user secret", secret);
 
-  // MARIO
-  // var userId = "5264",
-  //     secret = "aaqRcNdJox8Js4mWYI2KO8MAhu/rDJ3DrP0oe5yljyd0qAzYAKLhzy6FdIuFgANFVhBT6h5muZ+BCqzqtPBz2Q==";
-
-  // ME
-  var userId = "5521",
-      secret = "FRQtr1NvxfI3MVtitozN3RgCfFwZC8PQfJNnHbH672rc7+NpmHQGYc94b+nhK0KMbWLuU7Ps+ccs7msOwlgBSA==";
-
-  // CHINH
-  // var userId = "5712",
-  //     secret = "wmCIAW/DSwaLTB4f8L023vkRnpF5CwIiHYL0U+1KCJG9LedS/q8j42ohvo80hbcIOofxKXpIHsruCKYNsE5qfA==";
-
-  // CHINH 2
-  // var userId = "5607",
-  //     secret = "iP9TgR+0S1L3CG4NahlgMENu1lZK+9YQqlzYNuyDAtPowqGxGhqQl46RBE6nPsPDaGJbwfLbuJ960jhL96krTw==";
-
-  // JBIT
-  // var userId = "16892",
-  //     secret = "/pY6BZz0D5RnrajQmvq8X+tUYp/LegPOIxZQa381s58SQHxLfcm/s8fHAPHz2eu30gKzJJTuYXSWMnRRNCa8mA==";
+  // production
+  // var userId = "17284",
+  //     secret = "h5SE8LtTDY8hadMy8WCObaY4p0Bm7xnFEsUp12kEsVVKOIaNWlOVth7axWnKgDbKuaamKipWP3Z90K076JlKfA==";
 
   //Sign the request for Authentication
   $.ajaxSetup({
@@ -40,8 +26,8 @@ $(document).ready(function() {
   //end
 
   button.click(function(){
-    button.text('Logging in ...');
-    button.prop('disabled', true);
+    // button.text('Logging in ...');
+    // button.prop('disabled', true);
     //Request login token
     $.getJSON(quoineHost + "/cors_auth", function(response) {
       //Use token to login to trading dashboard
